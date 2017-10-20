e45586c5f22f3c3b05544574a7898cc41b137bdb
beingrahuljha <being.rkj.007@gmail.com>
2017-10-21 05:11:45 +0530

    Add files via upload

---
+++ myJson.js
@@ -0,0 +1,266 @@
+var flipData = {
+    "productData": [
+        {
+            "ptoductId": 1,
+            "productCategory": "Smartphones",
+            "name": "Xiaomi Mi Mix 2",
+            "brand": "Xiaomi",
+            "devCodeName": "Chiron",
+            "image": "https://goo.gl/cqTwch",
+            "specifications": {
+                "General": {
+                    "In The Box": "Handset, Handset Case, USB Cable, Adapter, SIM Tray Remover Pin, User Guide, Warranty Card",
+                    "Model Name": "Mi Mix 2",
+                    "Color": "Black",
+                    "SIM Type": "Dual Sim",
+                    "Hybrid Sim Slot": "No",
+                    "Touchscreen": "Yes",
+                    "OTG Compatible": "Yes",
+                    "Quick Charging": "Not specified",
+                    "Sound Enhancements": "Speaker: One (Bottom Opening), Headphone Jack: USB Headphone, Microphones: 3 (Noise Cancellation)"
+                },
+                "displayFeatures": {
+                    "Display Size": "5.99 inch",
+                    "Resolution": "2160 x 1080 Pixels",
+                    "Resolution Type": "Full HD+",
+                    "GPU": "Adreno 540",
+                    "Display Type": "IPS",
+                    "Other Display Features": "Contrast Ratio: 1500:1, Resolution Type: Full HD+"
+                },
+                "Os&processorFeatures": {
+                    "Operating System": "Android Nougat7.1.1",
+                    "Processor Type": "Qualcomm Snapdragon835 Octa Core2.5 GHz",
+                    "Processor Core": "Octa Core",
+                    "Primary Clock Speed": "2.5 GHz"
+                },
+                "memory&StorageFeatures": {
+                    "Internal Storage": "64 GB",
+                    "RAM": "4 GB"
+                },
+                "cameraFeatures": {
+                    "Primary Camera Available": "Yes",
+                    "Primary Camera": "12MP",
+                    "Primary Camera Features": "Aperture: f/2.0, 1.25 microns Pixel Size, 4-axis, OIS, 6p Lens, PDAF",
+                    "Secondary Camera Available": "Yes",
+                    "Secondary Camera": "5MP",
+                    "Secondary Camera Features": "Aperture: f2.0, 1.12 microns Pixel Size, 4p Lens",
+                    "Flash": "Rear Dual Tone Flash",
+                    "Video Recording": "Yes"
+                },
+                "connectivityFeatures": {
+                    "Network Type": "4G VOLTE, 3G, 2G",
+                    "Supported Networks": "4G VoLTE, UMTS, GSM, 4G LTE, WCDMA",
+                    "Internet Connectivity": "4G, 3G, Wi-Fi",
+                    "3G": "Yes",
+                    "Bluetooth Support": "Yes",
+                    "Bluetooth Version": 5,
+                    "Wi-Fi": "Yes",
+                    "Wi-Fi Version": "802.11 a/b/g/n/ac",
+                    "NFC": "Yes",
+                    "USB Connectivity": "Yes",
+                    "Map Support": "Google Maps",
+                    "GPS Support": "Yes"
+                },
+                "otherDetails": {
+                    "Smartphone": "Yes",
+                    "SIM Size": "Nano SIM and Nano SIM",
+                    "SMS": "Yes",
+                    "Graphics PPI": "403 PPI",
+                    "Internal Memory": "128 GB",
+                    "Sensors": "Fingerprint Scanner, Ambient Light Sensor, Proximity Sensor, E-compass, Accelerometer, Gyroscope, Hall Sensor, Barometer",
+                    "Other Features": "10 nm Architecture, UFS 2.0, Body: Metal Frame, Ceramic Body, USB: Type C"
+                },
+                "battery&PowerFeatures": {
+                    "Battery Capacity": "3400 mAh",
+                    "Battery Type": "Li-Polymer"
+                },
+                "Dimensions": {
+                    "Width": "75.49 mm",
+                    "Height": "151.86 mm",
+                    "Depth": "7.7 mm",
+                    "Weight": "185 g"
+                },
+                "Warranty": {
+                    "Warranty Summary": "1 Year on Handset, 6 Months on Accessories"
+                }
+            },
+            "mrp": 37999,
+            "discount": 5,
+            "currentPrice": 35999,
+            "state": "In Stock",
+            "Url": "https://goo.gl/8B7Sfa",
+            "flipkartAssured": "Yes"
+        },
+        {
+            "productId": 2,
+            "name": "Samsung Galaxy s8 plus",
+            "brand": "Samsung",
+            "devCodeName": null,
+            "image": "https://goo.gl/Vm4fLk",
+            "specifications": {
+                "General": {
+                    "In The Box": "Handset, Earphones Tuned by AKG, USB Type-C Cable, Travel Adapter, USB Connector (C to A), Micro USB Connector (C to B), Ejector Pin, Quick Start and Smart Switch Leaflet",
+                    "Model Number": "SM-G955FZKDINS",
+                    "Model Name": "Galaxy S8 Plus",
+                    "Color": "Midnight Black",
+                    "SIM Type": "Dual Sim",
+                    "Hybrid Sim Slot": "Yes",
+                    "Touchscreen": "Yes",
+                    "OTG Compatible": "Yes",
+                    "Quick Charging": "Yes"
+                },
+                "displayFeatures": {
+                    "Display Size": "6.2 inch",
+                    "Resolution": "2960 x 1440 pixels",
+                    "Resolution Type": "Quad HD+",
+                    "Display Type": "Super AMOLED",
+                    "Other Display Features": "Corning Gorilla Glass 5"
+                },
+                "os&ProcessorFeatures": {
+                    "Operating System": "Android Nougat 7",
+                    "Processor Type": "Exynos 8895 Octa Core 2.3GHz",
+                    "Processor Core": "Octa Core",
+                    "Primary Clock Speed": "2.3 GHz"
+                },
+                "memory&StorageFeatures": {
+                    "Internal Storage": "64 GB",
+                    "RAM": "4 GB"
+                },
+                "cameraFeatures": {
+                    "Primary Camera Available": "Yes",
+                    "Primary Camera": "12MP",
+                    "Primary Camera Features": "Dual Pixel OIS (Optical image Stabilization)",
+                    "Secondary Camera Available": "Yes",
+                    "Secondary Camera": "8MP",
+                    "Secondary Camera Features": "Virtual Shot",
+                    "Flash": "Single LED Flash (Rear)",
+                    "Video Recording": "Yes"
+                },
+                "connectivityFeatures": {
+                    "Network Type": "4G, 3G, 2G",
+                    "Supported Networks": "4G LTE, 4G LTE",
+                    "Internet Connectivity": "4G, 3G, Wi-Fi",
+                    "3G": "Yes",
+                    "Bluetooth Support": "Yes",
+                    "Bluetooth Version": "v5.0",
+                    "Wi-Fi": "Yes",
+                    "USB Connectivity": "Yes"
+                },
+                "otherDetails": {
+                    "Smartphone": "Yes",
+                    "SIM Size": "Nano SIM (4FF)",
+                    "SMS": "Yes",
+                    "Internal Memory": "64 GB",
+                    "Sensors": "Accelerometer, Barometer, Fingerprint Sensor, Gyro Sensor, Geomagnetic Sensor, Hall Sensor, HR Sensor, Proximity Sensor, RGB Light Sensor, Iris Sensor, Pressure Sensor",
+                    "Other Features": "LTE Cat 16 Capable, Water and Dust Resistance: IP68 Compatible, Fast Charging on Wired and Wireless, Wireless Charging Compatible with WPC and PMA"
+                },
+                "battery&PowerFeatures": {
+                    "Battery Capacity": "3500 mAh",
+                    "Battery Type": "Li-ion"
+                },
+                "Dimensions": {
+                    "Width": "73.4 mm",
+                    "Height": "159.5 mm",
+                    "Depth": "8.1 mm"
+                },
+                "Warranty": {
+                    "Warranty Summary": "1 Year for Mobile & 6 Months Accessories Warranty"
+                },
+                "mrp": 64900,
+                "discount": 9,
+                "currentPrice": 58900,
+                "state": "In Stock",
+                "Url": "https://goo.gl/NUQJf7",
+                "flipkartAssured": "Yes"
+            }
+        }
+    ],
+    "userData": [
+        {
+            "customerprivilege": "normal",
+            "firstName": "Rahul",
+            "lastName": "Jha",
+            "email": "rahuljha@gmail.com",
+            "recentlyViewed": [
+                " Mi Mix 2 ",
+                " Samsung Galaxy s8 plus"
+            ],
+            "productsPurchased": [
+                "Redmi Note 3",
+                " Hardcover for Redmi Note 3"
+            ],
+            "wishlist": [
+                "samsung galaxy s8 plus",
+                "Apple Iphone 8 plus"
+            ],
+            "dispatchedcity": "Mumbai",
+            "Pincode": 421101,
+            "Phone": 7890112233
+        },
+        {
+            "customerprivilege": "normal",
+            "firstName": "Rohan",
+            "lastName": "Jha",
+            "email": "rohanjha@gmail.com",
+            "recentlyViewed": [
+                "United Colors of Benetton Men Corporate Casuals",
+                "Apple Iphone 8 plus"
+            ],
+            "productsPurchased": [
+                "Redmi 4A",
+                "Backcover for Redmi 4A"
+            ],
+            "wishlist": [
+                "Mi Mix 2"
+            ],
+            "dispatchedcity": "Kolkata",
+            "Pincode": 700061,
+            "Phone": 7890223344
+        },
+        {
+            "customerprivilege": "flipkartfirst",
+            "firstName": "Anjali",
+            "lastName": "Jha",
+            "email": "anjalijha@gmail.com",
+            "recentlyViewed": [
+                "Mi Mix 2"
+            ],
+            "productsPurchased": [
+                "Redmi 4A",
+                "Flip cover for Redmi 4A"
+            ],
+            "wishlist": [
+                "samsung galaxy s8 plus",
+                "Apple Iphone 8 plus"
+            ],
+            "dispatchedcity": "Patna",
+            "Pincode": 847031,
+            "Phone": 7890334455
+        }
+    ]
+}
+
+var readData = function(flipData) {
+    console.log(flipData);
+};
+  readData(flipData);
+  
+function fullNameAge(userData, email) {
+    for (var i = 0; i < userData.length; i++) {
+      if (userData[i].email == email) {
+        return (
+          userData[i].firstName +
+          " " +
+          userData[i].lastName +
+          " " +
+          userData[i].age
+        );
+      } else {
+        return "User not found";
+      }
+    }
+}
+  
+var output = fullNameAge(userData, "rahuljha@gmail.com");
+console.log(output);
+  
