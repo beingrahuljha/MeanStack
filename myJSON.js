1143e19fbb220eb671bffd9b244bd2c212534914
beingrahuljha <being.rkj.007@gmail.com>
2017-10-25 04:03:58 +0530

    Add files via upload

---
+++ myJSON.js
@@ -0,0 +1,250 @@
+var flipData = {                                                                              //flipkart Json Datastore
+    "productData": [
+        {
+            "ptoductId": 1,
+            "productCategory": "Smartphones",
+            "name": "Xiaomi Mi Mix 2",
+            "brand": "Xiaomi",
+            "model no": "Mi Mix 2",
+            "devCodeName": "Chiron",
+            "image": "https://goo.gl/cqTwch",
+            "specifications": {
+                "General": {
+                    "In The Box": "Handset, Handset Case, USB Cable, Adapter, SIM Tray Remover Pin, User Guide, Warranty Card",
+                    "Model Name": "Mi Mix 2",
+                    "Color": "Black",
+                    "SIM Type": "Dual Sim",
+                    "Hybrid Sim Slot": "No",
+                    "Touchscreen": "Yes",
+                    "OTG Compatible": "Yes",
+                    "Sound Enhancements": "Speaker: One (Bottom Opening), Headphone Jack: USB Headphone, Microphones: 3 (Noise Cancellation)"
+                },
+                "displayFeatures": {
+                    "Display Size": "5.99 inch",
+                    "Resolution": "2160 x 1080 Pixels",
+                    "Resolution Type": "Full HD+",
+                    "GPU": "Adreno 540",
+                    "Display Type": "IPS",
+                    "Other Display Features": "Contrast Ratio: 1500:1, Resolution Type: Full HD+"
+                },
+                "Os&processorFeatures": {
+                    "Operating System": "Android Nougat7.1.1",
+                    "Processor Type": "Qualcomm Snapdragon835 Octa Core2.5 GHz",
+                    "Processor Core": "Octa Core",
+                    "Primary Clock Speed": "2.5 GHz"
+                },
+                "cameraFeatures": {
+                    "Primary Camera Available": "Yes",
+                    "Primary Camera": "12MP",
+                    "Primary Camera Features": "Aperture: f/2.0, 1.25 microns Pixel Size, 4-axis, OIS, 6p Lens, PDAF",
+                    "Secondary Camera Available": "Yes",
+                    "Secondary Camera": "5MP",
+                    "Secondary Camera Features": "Aperture: f2.0, 1.12 microns Pixel Size, 4p Lens",
+                    "Flash": "Rear Dual Tone Flash",
+                    "Video Recording": "Yes"
+                },
+                "connectivityFeatures": {
+                    "Network Type": "4G VOLTE, 3G, 2G",
+                    "Supported Networks": "4G VoLTE, UMTS, GSM, 4G LTE, WCDMA",
+                    "Internet Connectivity": "4G, 3G, Wi-Fi",
+                    "3G": "Yes",
+                    "Bluetooth Support": "Yes",
+                    "Bluetooth Version": 5,
+                    "Wi-Fi": "Yes",
+                    "Wi-Fi Version": "802.11 a/b/g/n/ac",
+                    "NFC": "Yes",
+                    "USB Connectivity": "Yes",
+                    "Map Support": "Google Maps",
+                    "GPS Support": "Yes"
+                },
+                "otherDetails": {
+                    "Smartphone": "Yes",
+                    "SIM Size": "Nano SIM and Nano SIM",
+                    "SMS": "Yes",
+                    "Graphics PPI": "403 PPI",
+                    "Internal Memory": "128",
+                    "Sensors": "Fingerprint Scanner, Ambient Light Sensor, Proximity Sensor, E-compass, Accelerometer, Gyroscope, Hall Sensor, Barometer",
+                    "Other Features": "10 nm Architecture, UFS 2.0, Body: Metal Frame, Ceramic Body, USB: Type C"
+                },
+                "battery&PowerFeatures": {
+                    "Battery Capacity": "3400 mAh"
+                },
+                "Dimensions": {
+                    "Width": "75.49 mm",
+                    "Height": "151.86 mm",
+                    "Depth": "7.7 mm",
+                    "Weight": "185 g"
+                },
+                "Warranty": {
+                    "Warranty Summary": "1 Year on Handset, 6 Months on Accessories"
+                }
+            },
+            "mrp": 37999,
+            "discount": 5,
+            "currentPrice": 35999,
+            "state": "In Stock",
+            "Url": "https://goo.gl/8B7Sfa",
+            "flipkartAssured": "Yes"
+        },
+        {
+            "productId": 2,
+            "productCategory": "Footwear",
+            "name": "United Colors of Benetton Men Corporate Casuals",
+            "brand": "United Colors of Benetton",
+            "image": "https://goo.gl/JSn9aB",
+            "availableSizes": [
+                8,
+                9
+            ],
+            "specifications": {
+                "General": {
+                    "Model Name": "Men Corporate Casuals",
+                    "Ideal For": "Men",
+                    "Occasion": "Casual",
+                    "Outer Material": "Canvas",
+                    "School Shoe": "No"
+                },
+                "productDetails": {
+                    "Weight": "382 g (per single Shoe) - Weight of the product may vary depending on size.",
+                    "Style": "Cut Work Detail, Panel and Stitch Detail, Textured Detail",
+                    "Tip Shape": "Round",
+                    "Season": "SS16",
+                    "Closure": "Laced"
+                }
+            },
+            "mrp": 3999,
+            "discount": 45,
+            "currentPrice": 2199,
+            "state": "In Stock",
+            "Url": "https://goo.gl/iwiK76",
+            "flipkartAssured": "Yes"
+        }
+    ],
+    "userData": [
+        {
+            "firstName": "Rahul",
+            "lastName": "Jha",
+            "age": 21,
+            "email": "rahuljha@gmail.com",
+            "customerprivilege": "normal",
+            "recentlyViewed": [
+                " Mi Mix 2 ",
+                " Samsung Galaxy s8 plus"
+            ],
+            "productsPurchased": [
+                "Redmi Note 3",
+                " Hardcover for Redmi Note 3"
+            ],
+            "wishlist": [
+                "samsung galaxy s8 plus",
+                "Apple Iphone 8 plus"
+            ],
+            "orders": {
+                "name": "The God Delusion",
+                "productCategory": "Books-NonFiction",
+                "productId": 5,
+                "sellingPrice": 400,
+                "status": "Out for Delivery"
+            },
+            "shippingAddress": "My Address",
+            "dispatchedcity": "Mumbai",
+            "Pincode": 421101,
+            "Phone": 7890112233
+        },
+        {
+            "firstName": "Rohan",
+            "lastName": "Jha",
+            "age": 16,
+            "email": "rohanjha@gmail.com",
+            "customerprivilege": "normal",
+            "recentlyViewed": [
+                "United Colors of Benetton Men Corporate Casuals",
+                "Apple Iphone 8 plus"
+            ],
+            "productsPurchased": [
+                "Redmi 4A",
+                "Backcover for Redmi 4A"
+            ],
+            "wishlist": [
+                "Mi Mix 2"
+            ],
+            "orders": {
+                "name": "The Complete Sherlock Holmes",
+                "productCategory": "Books-Fiction-Detective",
+                "productId": 4,
+                "sellingPrice": 700,
+                "status": "In Transit"
+            },
+            "shippingAddress": "His Address",
+            "dispatchedcity": "Kolkata",
+            "Pincode": 700061,
+            "Phone": 7890223344
+        },
+        {
+            "firstName": "Anjali",
+            "lastName": "Jha",
+            "age": 18,
+            "email": "anjalijha@gmail.com",
+            "customerprivilege": "flipkartfirst",
+            "recentlyViewed": [
+                "Mi Mix 2"
+            ],
+            "productsPurchased": [
+                "Redmi 4A",
+                "Flip cover for Redmi 4A"
+            ],
+            "wishlist": [
+                "samsung galaxy s8 plus",
+                "Apple Iphone 8 plus"
+            ],
+            "orders": {
+                "name": "Pigeon Toaster",
+                "productCategory": "Kitchen Appliances",
+                "productId": 3,
+                "sellingPrice": 1100,
+                "status": "Cancelled"
+            },
+            "shippingAddress": "Her Address",
+            "dispatchedcity": "Patna",
+            "Pincode": 847031,
+            "Phone": 7890334455
+        }
+    ]
+}
+
+
+
+
+var readData = function(flipData) {                                                          //first fuction
+  console.log(flipData);
+};
+
+readData(flipData);
+
+
+
+
+function fullNameAge(userData, email) {                                                      //second function
+  userData.forEach(function(user) {
+    if (user.email == email) {
+      console.log(
+        user.firstName +
+          " " +
+          user.lastName +
+          " " +
+          user.age +
+          " " +
+          user.orders.name +
+          " " +
+          user.orders.status
+      );
+    } else {
+      return "User not found";
+    }
+  });
+}
+fullNameAge(flipData.userData, "rahuljha@gmail.com");
+
+
+
+                                                                                             //third function Help!
